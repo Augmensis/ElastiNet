46eb0bc461583cfd02307a5eb30693ac983bbf94
Augmensis <derek.hampton@hotmail.com>
2014-08-26 17:16:53 +0100

    init

---
+++ ElastiNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElastiNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
